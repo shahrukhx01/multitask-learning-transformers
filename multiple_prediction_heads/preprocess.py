8f795b6ba2cbfe71626cbde7f24330d088008f09
shahrukhx01 <sk28671@gmail.com>
2021-09-26 22:14:28 +0200

    add multiple head multitask learning boilerplater

--- multiple_prediction_heads/preprocess.py
+++ multiple_prediction_heads/preprocess.py
@@ -5,6 +5,11 @@ def convert_to_features(example_batch, model_name="roberta-base", max_length=512
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
     inputs = list(example_batch["doc"])
 
-    features = tokenizer(inputs, max_length=max_length, padding=True)
+    features = tokenizer(
+        inputs,
+        max_length=max_length,
+        padding=True,
+        padding="max_length",
+    )
     features["labels"] = example_batch["target"]
     return features
